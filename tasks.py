07f88a43506f0ffe48c5363b7afba8b0fd31e896
Mahmoud Hossam <mahmoud.hossam@arabyads.com>
2019-02-03 15:27:58 +0200

    Fix imports

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 from celery_app import app
-from db import session, User
+from db import DB, User
+
+db = DB()
 
 
 @app.task
 def task(message):
     user = User(name=message["name"], email=message["email"])
-    session.add(user)
-    session.commit()
+    db.session.add(user)
+    db.session.commit()
