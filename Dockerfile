e561ec0c8a6e0c9a43b1170cefb97707da156526
Mahmoud Hossam <mahmoud.hossam@arabyads.com>
2019-02-03 17:30:38 +0200

    Add entrypoint

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,12 @@ COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
+COPY entrypoint.sh .
+
+RUN chmod +x entrypoint.sh
+
 USER queue
+
+ENTRYPOINT ["/app/entrypoint.sh"]
 
 CMD ["python", "processor.py"]
---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export DATABASE_URL="postgres://user:password@postgres:5432/users"
+
+postgres_ready() {
+python << END
+import sys
+
+import psycopg2
+
+try:
+    psycopg2.connect(
+        dbname="users",
+        user="user",
+        password="password",
+        host="postgres",
+        port="5432",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+
+END
+}
+until postgres_ready; do
+  >&2 echo 'Waiting for PostgreSQL to become available...'
+  sleep 1
+done
+>&2 echo 'PostgreSQL is available'
+
+python db.py
+
+exec "$@"
